ec8f6a0887e72a69250079f3b745d91eaeb890da
INTEL <sethj4774@gmail.com>
2024-05-17 16:00:31 +1000

    added functioning ontrack getTaskStatus

--- src/main/java/sit707_week10/OnTrack.java
+++ src/main/java/sit707_week10/OnTrack.java
@@ -1,7 +1,18 @@
 package sit707_week10;
 
 public class OnTrack {
-	public static String getTaskStatus(String winton) {
-		return null;
-	}
+    public static String getTaskStatus(String taskId) {
+        switch (taskId) {
+            case "task1":
+                return "Not Started";
+            case "task2":
+                return "Working On It";
+            case "task3":
+                return "Need Help";
+            case "task4":
+                return "Ready for Feedback";
+            default:
+                return "Not Started";
+        }
+    }
 }
